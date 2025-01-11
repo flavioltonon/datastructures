5556246b87a8a2e9f94dc4df2297132013bf7448
Flávio Tonon <flavioltonon@gmail.com>
2025-01-11 12:45:14 -0300

    chore: add SinglyLinkedList.Search example

--- examples/singly_linked_list_test.go
+++ examples/singly_linked_list_test.go
@@ -10,3 +10,13 @@ func ExampleSinglyLinkedList_Print() {
 	linkedList.Print()
 	// Output: a: 0 -> b: 1 -> c: 2 -> <nil>
 }
+
+func ExampleSinglyLinkedList_Search() {
+	linkedList := datastructures.NewSinglyLinkedList[int]()
+	linkedList.AppendNode(datastructures.NewNode("a", 0))
+	linkedList.AppendNode(datastructures.NewNode("b", 1))
+	linkedList.AppendNode(datastructures.NewNode("c", 2))
+	node := linkedList.Search("b")
+	node.Print()
+	// Output: b: 1
+}
