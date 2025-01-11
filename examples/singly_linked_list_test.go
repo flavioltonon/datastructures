059188aa606820248b028f6cf975c311b9079872
Flávio Tonon <flavioltonon@gmail.com>
2025-01-11 11:12:38 -0300

    refactor: transform cmd package into examples

--- examples/singly_linked_list_test.go
+++ examples/singly_linked_list_test.go
@@ -1,12 +1,13 @@
-package main
+package examples
 
 import "github.com/flavioltonon/datastructures"
 
-func main() {
+func ExampleSinglyLinkedList() {
 	linkedList := datastructures.NewSinglyLinkedList[int]()
 	linkedList.AppendNode(datastructures.NewNode("b", 1))
 	linkedList.AppendNode(datastructures.NewNode("c", 2))
 	linkedList.AppendNode(datastructures.NewNode("d", 3))
 	linkedList.PrependNode(datastructures.NewNode("a", 0))
 	linkedList.Traverse()
+	// Output: a: 0 -> b: 1 -> c: 2 -> d: 3 -> <nil>
 }
